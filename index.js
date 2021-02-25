de3d987bd6a80db18b545d82a2e9e578c331be84
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-02-25 10:09:08 -0800

    commit tree 1e4ffa6b1eb23d2ef46944006db81317334a35c5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Alex is the champion";
+newHeader.setAttribute("class", "victory");
